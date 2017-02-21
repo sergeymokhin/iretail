5711e6d4f9f1dcf82719af7078b2fd92e72e682a
Zver <Zver@ZVERDVD-9TQM4D9>
2017-02-21 14:09:28 +0700

    set brows ver

--- src/test/java/com/mycompany/iretailweb/features/search/IRetailIgorDebug_jenkins.java
+++ src/test/java/com/mycompany/iretailweb/features/search/IRetailIgorDebug_jenkins.java
@@ -61,6 +61,7 @@ public class IRetailIgorDebug_jenkins {
 // RemoteWebDriver webdriver2 = new RemoteWebDriver("http://localhost:4444/wd/hub", DesiredCapabilities.chrome());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("firefox_binary", "c:\\Program Files(x86)\\Mozilla Firefox\\Firefox.exe");
+        capabilities.setCapability("browser_version", "51.0.1");
         System.setProperty("webdriver.gecko.driver","c:\\geckodriver.exe");
         webdriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),DesiredCapabilities.firefox());
 //        webdriver = new ChromeDriver();
